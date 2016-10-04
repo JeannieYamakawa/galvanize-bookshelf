462206461612288b0b417621be7490762230e3be
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-04 16:31:11 -0500

    notes on users route

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,13 @@ const router = express.Router();
 router.post('/users', (req, res, next) => {
   console.log(req.body);
   bcrypt.hash(req.body.password, 12)
-    .then((hashed_password) => {
-      return knex('users').insert({first_name: req.body.firstName, last_name: req.body.lastName, email: req.body.email, hashed_password: hashed_password})
+    .then((hashed_password) => { //hashed_password is the same as "data"
+      return knex('users').insert({first_name: req.body.firstName, last_name: req.body.lastName, email: req.body.email, hashed_password: hashed_password})  //again, hashed_password is the same as "data:data"
       console.log(req.body.email, hashed_password);
     })
-    .then((users) => {
-      const user = users[0];
-      delete user.hashed_password;
+    .then((users) => {  //users is just a placeholder for the knex data
+      const user = users[0];    //this is the knex object returned in an array
+      delete user.hashed_password;  // The delete operator removes a property from an object. We're doing this is getting sent back to the front end and we don't want to send the hashed password back across the net.
       res.send('worked');
     })
     .catch((err) => {
